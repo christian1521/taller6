28bf1e9eb62970619729e819299a9a89e9860b28
christian1521 <christiantm1521@gmail.com>
2026-08-29 19:02:02 +0200

    Librerias

--- app/api.py
+++ app/api.py
@@ -24,7 +24,7 @@ def health() -> dict:
         name=settings.PROJECT_NAME, api_version=__version__, model_version=model_version
     )
 
-    return health.model_dump()
+    return model.model_dump() if hasattr(model, "model_dump") else model.dict()
 
 # Ruta para realizar las predicciones
 @api_router.post("/predict", response_model=schemas.PredictionResults, status_code=200)
